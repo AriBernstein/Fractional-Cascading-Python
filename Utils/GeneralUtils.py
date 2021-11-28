c0c115259f78a9f83323a6d42df5b258b2a7cf62
Ari Bernstein <aribernstein1997@gmail.com>
2021-11-27 23:00:17 -0500

    Add iterator to iterate through matrix columns, add method to return matrix column subset

--- Utils/GeneralUtils.py
+++ Utils/GeneralUtils.py
@@ -1,6 +1,34 @@
-from typing import Iterable
+from typing import Iterable, Iterator, List
 
 
+class ColIterator:
+    """
+    Given a matrix containing x lists of y items, iterate through items in the 
+    y dimension. """
+    
+    def __init__(self, data_set:Iterable[Iterable[object]], 
+                 sub_list_index:int) -> None:
+        self._matrix = data_set
+        self._i = 0
+        self._j = sub_list_index
+    
+    def __iter__(self) -> Iterator:
+        return self
+    
+    def __next__(self) -> object:
+        if self._i < len(self._matrix):
+            self._i += 1
+            return self._matrix[self._i - 1][self._j]
+        else:
+            raise StopIteration
+        
+
+def matrix_subset(matrix:Iterator[Iterator[object]], 
+                  l_index:int, r_index:int) -> List[List[object]]:
+    return [sub_list[l_index:r_index + 1] for sub_list in matrix]
+
+
+################### Utils for Pretty Printing Data Structures ##################
 def pretty_list(l:Iterable, opening_brace:chr='[', closing_brace:chr=']',
                 left_indent_len=0, line_len_limit=None) -> str:
     
