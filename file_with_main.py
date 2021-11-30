9527d381b5de97ab50c4ab1cf1f40102a922ddcc
Ari Bernstein <aribernstein1997@gmail.com>
2021-11-29 19:47:21 -0500

    Added option to rand_unique_ints method to place insert_val at either a random or constant location such that a random data set can be created to contain one FullNode with a single integer value for data and locations/usr/local/bin/python3

--- file_with_main.py
+++ file_with_main.py
@@ -14,11 +14,15 @@ if __name__ == "__main__":
     mins = [25, 25, 25]
     maxes = [75, 75, 75]
     
-    full_nodes = generate_FullNode_data_set(n, dim, loc_min, loc_max)
+    full_nodes = generate_FullNode_data_set(n, dim, loc_min, loc_max, 50)
     
     for i in full_nodes:
         print(i)
     
     rt = RangeTree(full_nodes, dim)
+    target = 50
+    print(f"Target: {target}")
     
     print(visualize_range_tree(rt))
+    
+    print(rt.query(target))
